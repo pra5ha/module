3c99971f5441048bc2f6647bd84190a51204c72d
pra5ha <47806864+pra5ha@users.noreply.github.com>
2019-04-29 15:36:04 +0530

    Update main.tf

--- watermarker/main.tf
+++ watermarker/main.tf
@@ -2,7 +2,7 @@ module "watermarker" {
   source        = "../module"
   service_name  = "watermarker"
   instance_type = "t2.micro"
-  desired_count = "1"
-  min_size      = "1"
-  max_size      = "2"
+  desired_count = "2"
+  min_size      = "2"
+  max_size      = "4"
 }
