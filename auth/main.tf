64aac475aa82aea3787a6d7e23f3c228cc5aaffe
pra5ha <47806864+pra5ha@users.noreply.github.com>
2019-04-29 14:21:26 +0530

    Update main.tf

--- auth/main.tf
+++ auth/main.tf
@@ -2,7 +2,6 @@ module "auth" {
   source        = "../module"
   service_name  = "auth"
   instance_type = "t2.micro"
-  ami_id        = ""
   desired_count = "1"
   min_size      = "1"
   max_size      = "2"
