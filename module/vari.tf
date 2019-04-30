4063b3ac6c588081e973567ccba088f904e97bfb
pra5ha <47806864+pra5ha@users.noreply.github.com>
2019-04-30 13:27:24 +0530

    Update vari.tf

--- module/vari.tf
+++ module/vari.tf
@@ -1,13 +1,16 @@
 variable "region" {
   default = "us-west-2"
 }
-
-variable "security_groups" {
-  default = "sg-0ae26b0d844a1f4b3"
+variable "avaliblity_zone" {
+  type    = "list"
+  default = ["us-west-2a", "us-west-2b"]
 }
 
 variable "subnet_id" {
   default = "subnet-0d4a269bf1aa7051c"
+}
+variable "security_groups" {
+  default = "sg-0ae26b0d844a1f4b3"
 }
 
 variable "load_balancers" {
@@ -29,12 +32,6 @@ variable "min_size" {
 variable "max_size" {
   default = "4"
 }
-
-variable "avaliblity_zone" {
-  type    = "list"
-  default = ["us-west-2a", "us-west-2b"]
-}
-
 variable "iam_role" {
   default = "admin_access"
 }
@@ -42,7 +39,6 @@ variable "iam_role" {
 variable "service_name" {
   default = "auth"
 }
-
 variable "user_data" {
   default = "user_data.sh"
 }
